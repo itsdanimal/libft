e3986dbef0c7d7ba113c1c86c542f971beeb85c9
Daniel Li <danli@e1z2r6p4.42.us.org>
2019-10-10 15:12:41 -0700

    strnequfix

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: danli <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 09:52:31 by danli             #+#    #+#             */
-/*   Updated: 2019/09/25 10:13:42 by danli            ###   ########.fr       */
+/*   Updated: 2019/10/10 15:12:24 by danli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 		return (s1 == s2 ? 1 : 0);
 	if (ft_strlen((char*)s1) != ft_strlen((char*)s2) && i < n)
 		return (0);
-	while (s1[i] && s2[i] && i < n)
+	while (s1[i] && s2[i] && i < n && (s1[i] != '\0' && s1[i] != '\0'))
 	{
 		if (s1[i] != s2[i])
 			return (0);
